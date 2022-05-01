f6beb0a31be889122e4ebe2900c8602538f17307
Vincenzofdg <vfdgiacomo@gmail.com>
2022-05-01 20:31:08 -0300

    Sync

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import { Switch, Route } from 'react-router-dom';
 
 // Imports from ./pages/:
 import Login from './pages/Login';
-import Home from './pages/Home';
 import Search from './pages/Search';
 import Album from './pages/Album';
-import Favorites from './pages/Favorites';
-import Profile from './pages/Profile';
-import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
 
 class App extends Component {
@@ -16,12 +12,8 @@ class App extends Component {
     return (
         <Switch>
           <Route exact path="/" component={ Login } />
-          <Route path="/home" component={ Home } />
           <Route path="/search" component={ Search } />
           <Route path="/album/:id" component={ Album } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route exact path="/profile" component={ Profile } />
-          <Route exact path="/profile/edit" component={ ProfileEdit } />
           <Route path="" component={ NotFound } />
         </Switch>
     );
